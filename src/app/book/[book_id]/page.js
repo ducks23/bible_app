4ad5056565585fcb978780e31f3c3f2894e11475
Jesse Leonard <jesse1517@gmail.com>
2024-03-02 23:18:30 -0600

    update caching

--- src/app/book/[book_id]/page.js
+++ src/app/book/[book_id]/page.js
@@ -3,10 +3,7 @@ import { BiblePage } from "@/components/Book";
 import { fetchBibleData } from "@/utils/api";
 
 export default async function Page({ params: { book_id } }) {
-  const book_name = "genesis";
-
-  fetchBibleData("book", book_name);
-  const data = await fetchBibleVerse(book_id);
+  const data = await fetchBibleData("book", book_id);
 
   return (
     <div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,39 +1,15 @@
 import MainBible from "@/sections/main";
 import { BiblePage } from "@/components/Book";
 
-async function fetchBibleBook(book) {
-  const baseUrl = "https://django-bible.vercel.app/book";
-  const data = {
-    book_name: book,
-    password: "2UuW4Mi8.bG2BGKpbAzXRr",
-  };
-  const jsonData = JSON.stringify(data);
+import { fetchBibleData } from "@/utils/api";
 
-  try {
-    const response = await fetch(baseUrl, {
-      method: "POST",
-      next: {
-        revalidate: 10,
-      },
-      body: jsonData, // Attach the serialized data
-    });
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    throw error; // Rethrowing the error for the caller to handle
-  }
-}
 export default async function Home() {
-  const data = await fetchBibleBook("genesis");
-  console.log(data);
+  const bible_name = "genesis";
+  const data = await fetchBibleData(bible_name);
   return (
     <div className="bg-sky-50">
       {" "}
-      <BiblePage chapter_id={"genesis"} data={data} />
+      <BiblePage chapter_id={bible_name} data={data} />
       <MainBible />
     </div>
   );
