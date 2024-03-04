6a214ebe21b307d9f7005ac1f835e85fe579bbac
Jesse Leonard <jesse1517@gmail.com>
2024-03-04 15:30:33 -0600

    made verse view work

---
+++ src/app/book/[book_id]/chapter/[chapter_id]/verse/[verse_id]/page.js
@@ -0,0 +1,19 @@
+import { fetchBibleData } from "@/utils/api";
+import { BiblePage } from "@/components/Book";
+
+export default async function Page({
+  params: { book_id, chapter_id, verse_id },
+}) {
+  const data = await fetchBibleData("verse", book_id, chapter_id, verse_id);
+  return (
+    <div>
+      <BiblePage
+        chapter_id={book_id}
+        chapter_number={chapter_id}
+        verse_number={verse_id}
+        data={data}
+        is_chapter={true}
+      />
+    </div>
+  );
+}
--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -11,153 +11,19 @@ import {
 } from "@/components/ui/select";
 import { useState } from "react";
 
+import {
+  bibleBooksChapters,
+  booksOfTheBible,
+  verse_counts,
+} from "@/helpers/data";
+import { fetchBibleData } from "@/utils/api";
+
 import { Button } from "@/components/ui/button";
-
-const booksOfTheBible = [
-  // Old Testament
-  { id: "Book", name: "Book", chapters: 1 },
-  { id: "genesis", name: "Genesis", chapters: 50 },
-  { id: "exodus", name: "Exodus", chapters: 40 },
-  { id: "leviticus", name: "Leviticus", chapters: 27 },
-  { id: "numbers", name: "Numbers", chapters: 36 },
-  { id: "deuteronomy", name: "Deuteronomy", chapters: 34 },
-  { id: "joshua", name: "Joshua", chapters: 24 },
-  { id: "judges", name: "Judges", chapters: 21 },
-  { id: "ruth", name: "Ruth", chapters: 4 },
-  { id: "1samuel", name: "1 Samuel", chapters: 31 },
-  { id: "2samuel", name: "2 Samuel", chapters: 24 },
-  { id: "1kings", name: "1 Kings", chapters: 22 },
-  { id: "2kings", name: "2 Kings", chapters: 25 },
-  { id: "1chronicles", name: "1 Chronicles", chapters: 29 },
-  { id: "2chronicles", name: "2 Chronicles", chapters: 36 },
-  { id: "ezra", name: "Ezra", chapters: 10 },
-  { id: "nehemiah", name: "Nehemiah", chapters: 13 },
-  { id: "esther", name: "Esther", chapters: 10 },
-  { id: "job", name: "Job", chapters: 42 },
-  { id: "psalms", name: "Psalms", chapters: 150 },
-  { id: "proverbs", name: "Proverbs", chapters: 31 },
-  { id: "ecclesiastes", name: "Ecclesiastes", chapters: 12 },
-  { id: "songofsolomon", name: "Song of Solomon", chapters: 8 },
-  { id: "isaiah", name: "Isaiah", chapters: 66 },
-  { id: "jeremiah", name: "Jeremiah", chapters: 52 },
-  { id: "lamentations", name: "Lamentations", chapters: 5 },
-  { id: "ezekiel", name: "Ezekiel", chapters: 48 },
-  { id: "daniel", name: "Daniel", chapters: 12 },
-  { id: "hosea", name: "Hosea", chapters: 14 },
-  { id: "joel", name: "Joel", chapters: 3 },
-  { id: "amos", name: "Amos", chapters: 9 },
-  { id: "obadiah", name: "Obadiah", chapters: 1 },
-  { id: "jonah", name: "Jonah", chapters: 4 },
-  { id: "micah", name: "Micah", chapters: 7 },
-  { id: "nahum", name: "Nahum", chapters: 3 },
-  { id: "habakkuk", name: "Habakkuk", chapters: 3 },
-  { id: "zephaniah", name: "Zephaniah", chapters: 3 },
-  { id: "haggai", name: "Haggai", chapters: 2 },
-  { id: "zechariah", name: "Zechariah", chapters: 14 },
-  { id: "malachi", name: "Malachi", chapters: 4 },
-
-  // New Testament
-  { id: "matthew", name: "Matthew", chapters: 28 },
-  { id: "mark", name: "Mark", chapters: 16 },
-  { id: "luke", name: "Luke", chapters: 24 },
-  { id: "john", name: "John", chapters: 21 },
-  { id: "acts", name: "Acts", chapters: 28 },
-  { id: "romans", name: "Romans", chapters: 16 },
-  { id: "1corinthians", name: "1 Corinthians", chapters: 16 },
-  { id: "2corinthians", name: "2 Corinthians", chapters: 13 },
-  { id: "galatians", name: "Galatians", chapters: 6 },
-  { id: "ephesians", name: "Ephesians", chapters: 6 },
-  { id: "philippians", name: "Philippians", chapters: 4 },
-  { id: "colossians", name: "Colossians", chapters: 4 },
-  { id: "1thessalonians", name: "1 Thessalonians", chapters: 5 },
-  { id: "2thessalonians", name: "2 Thessalonians", chapters: 3 },
-  { id: "1timothy", name: "1 Timothy", chapters: 6 },
-  { id: "2timothy", name: "2 Timothy", chapters: 4 },
-  { id: "titus", name: "Titus", chapters: 3 },
-  { id: "philemon", name: "Philemon", chapters: 1 },
-  { id: "hebrews", name: "Hebrews", chapters: 13 },
-  { id: "james", name: "James", chapters: 5 },
-  { id: "1peter", name: "1 Peter", chapters: 5 },
-  { id: "2peter", name: "2 Peter", chapters: 3 },
-  { id: "1john", name: "1 John", chapters: 5 },
-  { id: "2john", name: "2 John", chapters: 1 },
-  { id: "3john", name: "3 John", chapters: 1 },
-  { id: "jude", name: "Jude", chapters: 1 },
-  { id: "revelation", name: "Revelation", chapters: 22 },
-];
-const bibleBooksChapters = {
-  genesis: 50,
-  exodus: 40,
-  leviticus: 27,
-  numbers: 36,
-  deuteronomy: 34,
-  joshua: 24,
-  judges: 21,
-  ruth: 4,
-  "1samuel": 31,
-  "2samuel": 24,
-  "1kings": 22,
-  "2kings": 25,
-  "1chronicles": 29,
-  "2chronicles": 36,
-  ezra: 10,
-  nehemiah: 13,
-  esther: 10,
-  job: 42,
-  psalms: 150,
-  proverbs: 31,
-  ecclesiastes: 12,
-  songofsolomon: 8,
-  isaiah: 66,
-  jeremiah: 52,
-  lamentations: 5,
-  ezekiel: 48,
-  daniel: 12,
-  hosea: 14,
-  joel: 3,
-  amos: 9,
-  obadiah: 1,
-  jonah: 4,
-  micah: 7,
-  nahum: 3,
-  habakkuk: 3,
-  zephaniah: 3,
-  haggai: 2,
-  zechariah: 14,
-  malachi: 4,
-  matthew: 28,
-  mark: 16,
-  luke: 24,
-  john: 21,
-  acts: 28,
-  romans: 16,
-  "1corinthians": 16,
-  "2corinthians": 13,
-  galatians: 6,
-  ephesians: 6,
-  philippians: 4,
-  colossians: 4,
-  "1thessalonians": 5,
-  "2thessalonians": 3,
-  "1timothy": 6,
-  "2timothy": 4,
-  titus: 3,
-  philemon: 1,
-  hebrews: 13,
-  james: 5,
-  "1peter": 5,
-  "2peter": 3,
-  "1john": 5,
-  "2john": 1,
-  "3john": 1,
-  jude: 1,
-  revelation: 22,
-};
-
 const BiblePage = ({
   chapter_id,
   data,
   chapter_number,
+  verse_number,
   is_chapter = false,
 }) => {
   const router = useRouter();
@@ -167,13 +33,16 @@ const BiblePage = ({
   if (chapter_number == null) {
     chapter_number = "Chapter";
   }
+  if (verse_number == null) {
+    verse_number = "Verse";
+  }
   const [value, setValue] = useState(chapter_id);
   const [chapter, setChapter] = useState(chapter_number);
-  console.log(value);
+  const [verse, setVerse] = useState(verse_number);
 
   let size = bibleBooksChapters[value];
   function generateList(size) {
-    let list = ["Chapter"];
+    let list = [];
     for (let i = 1; i <= size; i++) {
       list.push(i);
     }
@@ -182,7 +51,9 @@ const BiblePage = ({
   let num_chapters = generateList(size);
 
   const redirectToAnotherPage = () => {
-    router.push(`/book/${value}/chapter/${chapter}`);
+    if (value != "Book" && chapter != "Chapter" && verse != "Verse") {
+      router.push(`/book/${value}/chapter/${chapter}/verse/${verse}`);
+    }
   };
   let title;
   if (!isNaN(parseInt(chapter_id.charAt(0)))) {
@@ -194,8 +65,12 @@ const BiblePage = ({
   } else {
     title = chapter_id.charAt(0).toUpperCase() + chapter_id.slice(1);
   }
-  console.log(value);
-  console.log(chapter);
+
+  let verses_list = ["Verses"];
+  if (chapter != "Chapter") {
+    let verse_nums = verse_counts[value.toString()][chapter.toString()];
+    verses_list = generateList(parseInt(verse_nums));
+  }
 
   return (
     <div className="flex justify-center bg-background h-fit flex-col">
@@ -243,16 +118,22 @@ const BiblePage = ({
           </Select>
         </div>
         <div className="p-1">
-          <Select>
+          <Select
+            type="single"
+            value={verse}
+            defaultValue={verse}
+            onValueChange={(value) => {
+              setVerse(value);
+            }}
+          >
             <SelectTrigger className="w-[100px]">
-              <SelectValue placeholder="Verse" />
+              <SelectValue placeholder="Verse">{verse} </SelectValue>
             </SelectTrigger>
             <SelectContent>
               <SelectGroup>
-                <SelectItem value="apple">Matthew</SelectItem>
-                <SelectItem value="banana">Mark</SelectItem>
-                <SelectItem value="blueberry">Luke</SelectItem>
-                <SelectItem value="grapes">John</SelectItem>
+                {verses_list.map((item, index) => (
+                  <SelectItem value={item}>{item}</SelectItem>
+                ))}
               </SelectGroup>
             </SelectContent>
           </Select>
