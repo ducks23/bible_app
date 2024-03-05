0e1dbdc0cdf16112e6961ffcde480460ab5ad8e4
Jesse Leonard <jesse1517@gmail.com>
2024-03-05 00:58:41 -0600

    analytics

--- src/app/page.js
+++ src/app/page.js
@@ -5,10 +5,10 @@ import { fetchBibleData } from "@/utils/api";
 
 export default async function Home() {
   const bible_name = "genesis";
-  const data = await fetchBibleData("book", bible_name);
+  const data = await fetchBibleData("chapter", bible_name, 1);
   return (
     <div className="">
-      <BiblePage chapter_id={bible_name} data={data} />
+      <BiblePage chapter_id={bible_name} chapter_number={1} data={data} />
     </div>
   );
 }
