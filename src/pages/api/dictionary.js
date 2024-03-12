0ca8f7660ede67bde340e32bccbe85bfb52e98ab
Jesse Leonard <jesse1517@gmail.com>
2024-03-12 00:51:29 -0500

    push

--- src/pages/api/dictionary.js
+++ src/pages/api/dictionary.js
@@ -1,17 +1,14 @@
 export default async function handler(req, res) {
   if (req.method === "POST") {
     var value = req.body.data;
-    fetch(
-      `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${value}\?key\=6f9b85d5-55eb-4cf2-8d3b-ed75d5d080a6 `,
-      {
-        method: "GET",
-        headers: {
-          Accept: "application/json",
-          "Access-Control-Allow-Origin": "*",
-          "Content-Type": "application/json",
-        },
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${value}`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
       },
-    )
+    })
       .then((response) => response.json())
       .then((data) => {
         console.log("here ", data);
